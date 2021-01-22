232d6814d606692ad31d120387f8cb54008076aa
rachitSinha2112 <71123839+rachitSinha2112@users.noreply.github.com>
2021-01-22 17:06:11 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,18 +18,25 @@ function setup() {
   ground1=new Ground(100,790,800,20);
   ground2=new Ground(10,400,20,800);
   ground3=new Ground(470,400,20,800);
-for(var k=0;k<=width;k=k+50){
-      divisions.push(new Division(k,height-divisionHeight/2,10,divisionHeight)); 
-  }
+  
+  for(var k=20;k<=width;k=k+70){
+    divisions.push(new Division(k,height-divisionHeight/2,10,divisionHeight)); 
+}
 
-for(var j=40;j<=width;j=j+50)
+  for(var j=40;j<=width;j=j+50)
 {   
   plinkos.push(new Plinko(j,75));
 }
-for(var j=40;j<=width-10;j=j+50){
+for(var j=80;j<=width-10;j=j+50){
   plinkos.push(new Plinko(j,175))
 }
-
+for(var j=40;j<=width;j=j+50)
+{   
+  plinkos.push(new Plinko(j,275));
+}
+for(var j=80;j<=width-10;j=j+50){
+  plinkos.push(new Plinko(j,375))
+}
 //   division1=new Division(30,1580,1560,20);
 // division2=new Division(400,1590,20,100);
 // division3=new Division(200,1590,20,500);
@@ -40,21 +47,26 @@ function draw() {
   background(0,0,0);  
   drawSprites();
   
-  
+  Engine.update(engine);
+  // division1.display();
+  // division2.display();
+  // division3.display();
+  // division4.display();
 
   ground1.display();
   ground2.display();
   ground3.display();
-
+  
   if (frameCount%60===0){
-      particles.push(new Particle(random(width/2-10,width/2+10),10,10));
+      particles.push(new Particle(random(20,450),10,10));
   }
  
-for(var i=0;i<plinkos.length;i++){
-plinkos[i].display();
-}
-  
-  
+  for(var i=0;i<plinkos.length;i++){
+     plinkos[i].display();
+  }
+
+
+
 for(var j=0;j<particles.length;j++){
     particles[j].display();
 }
