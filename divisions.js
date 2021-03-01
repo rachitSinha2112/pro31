544f03af750ecc42a28e41b886015d011388073f
rachitSinha2112 <71123839+rachitSinha2112@users.noreply.github.com>
2021-03-01 18:22:51 +0530

    Update divisions.js

--- divisions.js
+++ divisions.js
@@ -6,6 +6,7 @@ class Division{
      this.body=Bodies.rectangle(x,y,width,height,options)
     this.width=width;
      this.height=height;
+        World.add(world, this.body);
     }
     display(){
 var pos=this.body.position;
